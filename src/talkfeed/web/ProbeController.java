f15220b748362ae1fe20d6cabdf5f1f773468569
balmeyer <balmeyer@gmail.com>
2013-04-18 22:09:34 +0200

    add admin to probe

--- src/talkfeed/web/ProbeController.java
+++ src/talkfeed/web/ProbeController.java
@@ -15,10 +15,19 @@
  */
 package talkfeed.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 import talkfeed.cache.BlogCache;
 import talkfeed.cache.UserPresence;
@@ -27,7 +36,18 @@ import talkfeed.cache.UserPresence;
 public class ProbeController {
 
 	@RequestMapping(value = "/probe.htm", method = RequestMethod.GET)
-	public ModelAndView index() {
+	public ModelAndView index(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+       UserService userService = UserServiceFactory.getUserService();
+       User user = userService.getCurrentUser();
+	
+        if (user != null ){
+        	if (!userService.isUserAdmin()) {
+        		resp.sendRedirect("/index.htm");
+        	}
+        } else {
+        	resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+        }
 
 		ModelAndView mav = new ModelAndView("probe/index");
 
